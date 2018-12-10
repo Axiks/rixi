c97c1901558af44ad6bd5354a0d6414972fd327c
Niko <artwarchol@gmail.com>
2018-12-10 02:20:19 +0200

    add Update and Destroy Model/View

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -8,4 +8,23 @@ class MeetsController < ApplicationController
   def new
   	@meet = Meet.new
   end
+  def destroy
+  	@meet = Meet.find(params[:id])
+  	@meet.destroy
+  	redirect_to meets_path
+  end
+  def create
+  	@meet = Meet.new(meet_params)
+  	if @meet.save
+  		redirect_to @meet
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  def meet_params
+  	params.require(:meet).permit(:title, :description, :location, :meetdate, :meetime)
+  end
 end
